bf8b2f2c17c54cc66c4286e4ded78ee310d529e7
Bruce Chen <r08006@gmail.com>
2024-09-25 23:45:33 +0800

    clone line message code

--- src/main/java/idv/dev/brucey/Main.java
+++
@@ -1,7 +0,0 @@
-package idv.dev.brucey;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/idv/dev/brucey/project/enumeration/ResultStatusCode.java
@@ -0,0 +1,19 @@
+package idv.dev.brucey.project.enumeration;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+public enum ResultStatusCode {
+    DELETE_SUCCESS(200, "Delete operation succesfully."),
+    OK(200, "Success"),
+    SEND_MESSAGE_FAILURE(900, "Send message error."),
+    EXECUTE_LOCAL_SHELL_FAILURE(910, "Execute local shell file error.")
+    ;
+
+    private Integer code;
+    private String msg;
+
+}
